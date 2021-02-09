2321a3d4194a3541479f67920e2b7a6f17f0a7f2
LEE <LEE@DESKTOP-96FHJCO>
2021-02-09 10:49:04 +0900

    implement finding max days of the month (answer ver.)

--- src/honux/calendar/Calendar.java
+++ src/honux/calendar/Calendar.java
@@ -12,55 +12,16 @@ public class Calendar {
 		System.out.println("15 16  17  18  19 20 21");
 		System.out.println("22 23  24  25  26 27 28");
 		
-		// 수를 입력받아 최대일수를 구하는 프로그램
-		int a;
-		Scanner scanner = new Scanner(System.in);	
-		System.out.println("월을 입력하세요 : ");
-		String s1 = scanner.next();
-		a = Integer.parseInt(s1);
+		// 수를 입력받아 달의 최대일수를 출력하는 프로그램
+		System.out.println("달을 입력하세요 : ");
+		Scanner scanner = new Scanner(System.in);		
+		int month = scanner.nextInt();
 		
-		// 1,3,5,7,8,10,12월은 31일
-		// 2월은 28일
-		// 4,6,9,11월은 30일
-		int[] Day31 = new int[7];
-		Day31[0] = 1;
-		Day31[1] = 3;
-		Day31[2] = 5;
-		Day31[3] = 7;
-		Day31[4] = 8;
-		Day31[5] = 10;
-		Day31[6] = 12;
+		int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 		
-		int[] Day30 = new int[4];
-		Day30[0] = 4;
-		Day30[1] = 6;
-		Day30[2] = 9;
-		Day30[3] = 11;
-		
-		int[] Day28 = new int[1];
-		Day28[0] = 2;
-		
-		for(int i=0; i<Day31.length; i++) {
-			if(Day31[i] == a) {
-				System.out.printf("%d월은 31일까지 있습니다",a);
-				break;			
-			}
-		}
-		
-		for(int i=0; i<Day30.length; i++) {
-			if(Day30[i] == a) {
-				System.out.printf("%d월은 30일까지 있습니다",a);
-				break;			
-			}
-		}
-		
-		for(int i=0; i<Day28.length; i++) {
-			if(Day28[i] == a) {
-				System.out.printf("%d월은 28일까지 있습니다",a);
-				break;			
-			}
-		}
-		scanner.close();	
+		System.out.printf("%d월은 %d일까지 있습니다.", month, maxDays[month-1]);
+		scanner.close();
+	
 
 	}
 
